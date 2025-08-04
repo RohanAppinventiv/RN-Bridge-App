31076db5a91cf8246ceeca2b99f6da6940e3bd3d
Ankit Kumar <ankit.kumar4@appinventiv.com>
2025-08-04 16:20:47 +0530

    lib name import fix

--- generate-package.sh
+++ generate-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to generate react-native-emv-payment package
+# Script to generate quivio-transaction-processor package
 # This script clones the required folders and files to create the package structure
 
 set -e  # Exit on any error
@@ -26,26 +26,26 @@ fi
 echo "Creating $TARGET_DIR directory..."
 mkdir -p "$TARGET_DIR"
 
-# 1. Clone package_base_folder as react-native-emv-payment
+# 1. Clone package_base_folder as quivio-transaction-processor
 echo "Cloning package_base_folder..."
 cp -r package_base_folder/* "$TARGET_DIR/"
 
-# 2. Clone src root folder to react-native-emv-payment
+# 2. Clone src root folder to quivio-transaction-processor
 echo "Cloning src folder..."
 cp -r src "$TARGET_DIR/"
 
-# 3. Clone android/emvCardReaderLib and android/emvlib to react-native-emv-payment/libs
+# 3. Clone android/emvCardReaderLib and android/emvlib to quivio-transaction-processor/libs
 echo "Cloning emvCardReaderLib and emvlib..."
 mkdir -p "$TARGET_DIR/libs"
 cp -r android/emvCardReaderLib "$TARGET_DIR/libs/"
 cp -r android/emvlib "$TARGET_DIR/libs/"
 
-# 4. Clone android/app/src/main/java to react-native-emv-payment/libs/emvNative/src/main
+# 4. Clone android/app/src/main/java to quivio-transaction-processor/libs/emvNative/src/main
 echo "Cloning Java source files..."
 mkdir -p "$TARGET_DIR/libs/emvNative/src/main"
 cp -r android/app/src/main/java "$TARGET_DIR/libs/emvNative/src/main/"
 
-# 5. Create AndroidManifest.xml in react-native-emv-payment/libs/emvNative/src/main
+# 5. Create AndroidManifest.xml in quivio-transaction-processor/libs/emvNative/src/main
 echo "Creating AndroidManifest.xml..."
 cat > "$TARGET_DIR/libs/emvNative/src/main/AndroidManifest.xml" << 'EOF'
 <?xml version="1.0" encoding="utf-8"?>
